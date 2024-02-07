3234f64136a13236b60f7f0f7b84e4aad60fd258
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2024-02-07 20:29:34 +0400

    Initial Commit

---
+++ SubscriptionTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SubscriptionTracker
+//
+//  Created by Tako Menteshashvili on 07.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SubscriptionTracker/SubscriptionTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionTrackerApp.swift
+//  SubscriptionTracker
+//
+//  Created by Tako Menteshashvili on 07.02.24.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
